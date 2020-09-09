828dc50268ccd1ec070f0d8fb797d223deb0fc3b
Bow Young Kim <bowyoungk@BKs-MB.home>
2020-09-09 15:07:40 -0400

    add User model

--- models.py
+++ models.py
@@ -14,7 +14,7 @@ def connect_db(app):
 
 
 
-def User(db.Model): 
+class User(db.Model): 
     """Site user."""
 
     __tablename__ = "users"
@@ -23,24 +23,57 @@ def User(db.Model):
         db.String(20), 
         nullable=False,
         unique=True,
-        primary_key=True
-    )
-    password = db.Column(
-        db.Text,
-        nullable=False
-    )
-    email = db.Column(
-        db.String(50), 
-        nullable=False,
-        unique=True
-    )
-    first_name = db.Column(
-        db.String(30), 
-        nullable=False
-    )
-    last_name = db.Column(
-        db.String(30),
-        nullable=False
-    )
+        primary_key=True)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
 
-  
+    feedback = db.relationship("Feedback", backref="user", cascade="all,delete")
+    
+
+
+    @classmethod 
+    def register(cls, username, password, first_name, last_name, email)
+    """Register a user with hashed password & return user."""
+
+    hashed = bcrypt.generate_password_hash(password)
+    hashed_utf8 = hashed.decode("utf8")
+    user = cls(
+        username=username,
+        password=hashed_utf8,
+        first_name=first_name,
+        last_name=last_name,
+        email=email
+    )
+    db.session.add(user)
+    return user 
+
+
+    @classmethod 
+    def authenticate(cls, username, password)
+    """Validate that user exists & password is correct.
+    Return user of valid; else return false."""
+
+    user = User.query.filter_by(username=username).first()
+
+    if user and bcrypt.check_password_hash(user.password, password):
+        return user 
+    else:
+        return False 
+
+
+
+class Feedback(db.Model): 
+    """Feedback."""
+
+    __tablename__ = "feedback"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    username = db.Column(
+        db.String(20), 
+        db.ForeignKey('users.username'),
+        nullable=False
+    )
