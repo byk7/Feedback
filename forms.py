a926ee6d27d36d471fdde1fd1207d452d481d145
Bow Young Kim <bowyoungk@BKs-MB.home>
2020-09-09 15:40:18 -0400

    updated forms.py with FlaskForms

--- forms.py
+++ forms.py
@@ -5,3 +5,54 @@ from wtforms.validators import InputRequired, Length, NumberRange, Email, Option
 from flask_wtf import FlaskForm 
 
 
+class LoginForm(FlaskForm): 
+    """Login form"""
+
+    username = StringField(
+        "Username", 
+        validators=[InputRequired(), Length(min=1, max=20)],
+    )
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(), Length(min=6, max=30)], 
+    )
+
+class RegisterForm(FlaskForm): 
+    """Register form"""
+
+    username = StringField(
+        "Username", 
+        validators=[InputRequired(), Length(min=1, max=20)],
+    )
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(), Length(min=6, max=30)],
+    )
+    email = StringField(
+        "Email",
+        validators=[InputRequired(), Email(), Length(max=50)],
+    )
+    first_name = StringField(
+        "First Name", 
+        validators=[InputRequired(), Length(max=30)],
+    )
+    last_name = StringField(
+        "Last Name", 
+        validators=[InputRequired(), Length(max=30)],
+    )
+
+class FeedbackForm(FlaskForm):
+    """Add feedback form"""
+
+    title = StringField(
+        "Title",
+        validators=[InputRequired(), Length(max=100)],
+    )
+    content = StringField(
+        "Content", 
+        validators=[InputRequired()],
+    )
+
+class DeleteForm(FlaskForm):
+    """Delete form""" 
+    
